3fbaf7422330ebf9296e15ed2841c51afddb6d84
Chris Cox <chrisc@dyacon.com>
2020-04-17 10:22:26 -0600

    Reorganized project for packaging

---
+++ setup.py
@@ -0,0 +1,22 @@
+'''
+pymdl package setup
+'''
+
+import setuptools
+
+setuptools.setup(
+    name="pymdl",
+    version="0.2.2",
+    author="Chris Cox",
+    author_email="chrisrycx@gmail.com",
+    description="Python code for Dyacon MDL-700",
+    url="https://github.com/dyacon/pyMDL",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['pyyaml'],
+    python_requires='>=3.6'
+)
